b9c5b56933f1d7c62dca99edb58e913e7c792200
frankmaina-n <frankndiritu@gmail.com>
2023-03-23 14:09:55 -0700

    9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,25 +2,20 @@
 
 int main(void)
 {
-for (int i = 1; i <= 100; i++)
+int i;
+
+for (i = 1; i <= 100; i++)
 {
 if (i % 3 == 0 && i % 5 == 0)
-{
 printf("FizzBuzz ");
-}
 else if (i % 3 == 0)
-{
 printf("Fizz ");
-}
 else if (i % 5 == 0)
-{
 printf("Buzz ");
-}
 else
-{
 printf("%d ", i);
 }
-}
 printf("\n");
-return 0;
+
+return (0);
 }
