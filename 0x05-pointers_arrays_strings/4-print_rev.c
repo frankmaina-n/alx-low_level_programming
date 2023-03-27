403d74dbef7a5a0d5af9e551ab87d2a4bebb653b
frankmaina-n <frankndiritu@gmail.com>
2023-03-27 03:26:25 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * @s: the string to print
+ */
+void print_rev(char *s)
+{
+int i, len;
+
+len = 0;
+while (s[len] != '\0')
+len++;
+
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);
+
+_putchar('\n');
+}
