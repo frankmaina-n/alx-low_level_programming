daa8d23d1784c2ec367c33378812451621561b74
frankmaina-n <frankndiritu@gmail.com>
2023-03-27 02:59:25 -0700

    0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of an integer to 98
+ * @n: A pointer to the integer to be updated
+ *
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
+
