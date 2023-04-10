5f32d3f1a0a03dd6448a99f4d2cca2987da80409
frankmaina-n <frankndiritu@gmail.com>
2023-04-10 09:24:05 -0700

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,16 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
- * main - prints the number of arguments passed into it
- * @argc: the number of arguments passed
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: An array containing the command line arguments
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
-int main(int argc)
+int main(int argc, char *argv[])
 {
+(void) argv;
 printf("%d\n", argc - 1);
-
 return (0);
 }
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,6 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+int main(int argc, char *argv[]);
+
+#endif
